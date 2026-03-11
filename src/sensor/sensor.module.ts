bb1554ff606774d4f2dd70c3fc3aaeed7cfea258
alex <choijeaho86@gmail.com>
2026-03-11 17:53:51 +0900

    #29 sensor의 컬럼중 status이 MALFUNCTION으로 바뀌면 notic_emil유저들한데 알림 메일 전송

--- src/sensor/sensor.module.ts
+++ src/sensor/sensor.module.ts
@@ -7,9 +7,11 @@ import { SensorRepository } from './repository/sensor.repository';
 import { SensorController } from './sensor.controller';
 import { SensorService } from './sensor.service';
 import { SensorErrorModule } from 'src/sensor_error/sensor_error.module';
+import { EmailModule } from 'src/utils/service_email/email.module';
+import { NoticEmailModule } from 'src/notic_email/notic_email.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SensorEntity]), ServerErrorModule, TimerModule, SensorErrorModule],
+  imports: [TypeOrmModule.forFeature([SensorEntity]), ServerErrorModule, TimerModule, SensorErrorModule, EmailModule, NoticEmailModule],
   controllers: [SensorController],
   providers: [SensorService, SensorRepository],
   exports: [SensorService]
